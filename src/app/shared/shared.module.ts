9f8339ded0e44222c0c583c0e94b58ccdb1e203b
Salinas Cruz Edison <edison.salinasc@ug.edu.ec>
2023-01-05 23:49:59 +0000

    Estructura del modulo shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailInputComponent } from './components/inputs/email-input/email-input.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmailInputComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
